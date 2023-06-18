e22bda398352bc022ecbdf65659069c6f5851e22
Juarez Martins <juniorbmx13@gmail.com>
2023-06-18 17:54:13 -0300

    feat: projeto inicial

---
+++ src/main/java/com/ecoommercetreino/ecoomercetreino/EcoomerceJucaApplication.java
@@ -0,0 +1,13 @@
+package com.ecoommercetreino.ecoomercetreino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoomerceJucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoomerceJucaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoommercetreino/ecoomercetreino/EcoomerceJucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoommercetreino.ecoomercetreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoomerceJucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
