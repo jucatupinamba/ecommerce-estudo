e222fab1a870a382cf59b9ea0e1fb18dfa7fc8e9
Juarez Martins <juniorbmx13@gmail.com>
2023-06-24 15:08:10 -0300

    feat: criação da camada Service, implantação dos métodos findAll e findById - testes com Postman

--- src/main/java/com/ecoommercetreino/controller/UserController.java
+++ src/main/java/com/ecoommercetreino/controller/UserController.java
@@ -1,19 +1,33 @@
 package com.ecoommercetreino.controller;
 
 import com.ecoommercetreino.entities.User;
+import com.ecoommercetreino.services.UserService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria DB", "maria@email.com", "94327-0098", "swordfish");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+                return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
---
+++ src/main/java/com/ecoommercetreino/services/UserService.java
@@ -0,0 +1,25 @@
+package com.ecoommercetreino.services;
+
+import com.ecoommercetreino.entities.User;
+import com.ecoommercetreino.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById (Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
