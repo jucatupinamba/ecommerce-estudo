e32eb4adcebdc8dff8c25dd92f117164012119ad
Juarez Martins <juniorbmx13@gmail.com>
2023-07-03 18:31:20 -0300

    feat: associação many to many com JoinTable entre categoria e produto

--- src/main/java/com/ecoommercetreino/entities/Category.java
+++ src/main/java/com/ecoommercetreino/entities/Category.java
@@ -1,5 +1,6 @@
 package com.ecoommercetreino.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,7 +16,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){}
