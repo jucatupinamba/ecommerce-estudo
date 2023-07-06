e5c26680b01b515ed81180222e459397f2dfa8b1
Juarez Martins <juniorbmx13@gmail.com>
2023-07-06 20:23:35 -0300

    feat: criação entidade Payment, associação um para um com Order

--- src/main/java/com/ecoommercetreino/entities/Order.java
+++ src/main/java/com/ecoommercetreino/entities/Order.java
@@ -26,6 +26,8 @@ public class Order implements Serializable {
     private User client;
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order(){}
 
@@ -68,6 +70,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems(){
