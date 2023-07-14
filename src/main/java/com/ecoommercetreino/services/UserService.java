861abd6c3b48edab9a80829ef2500ed409820dce
Juarez Martins <juniorbmx13@gmail.com>
2023-07-13 23:01:17 -0300

    feat: tratamento de exceção no método findById

--- src/main/java/com/ecoommercetreino/services/UserService.java
+++ src/main/java/com/ecoommercetreino/services/UserService.java
@@ -2,6 +2,7 @@ package com.ecoommercetreino.services;
 
 import com.ecoommercetreino.entities.User;
 import com.ecoommercetreino.repositories.UserRepository;
+import com.ecoommercetreino.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById (Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/ecoommercetreino/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ecoommercetreino.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id){
+        super("Resource not found " + id);
+    }
+}
