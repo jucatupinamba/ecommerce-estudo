0a6a45e3679324c41b45740039a5d07638159b77
Juarez Martins <juniorbmx13@gmail.com>
2023-07-17 07:50:09 -0300

    feat: tratamento de exceção método update

--- src/main/java/com/ecoommercetreino/services/UserService.java
+++ src/main/java/com/ecoommercetreino/services/UserService.java
@@ -4,6 +4,7 @@ import com.ecoommercetreino.entities.User;
 import com.ecoommercetreino.repositories.UserRepository;
 import com.ecoommercetreino.services.exceptions.DataBaseException;
 import com.ecoommercetreino.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
