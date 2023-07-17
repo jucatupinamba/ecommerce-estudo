23d4bcebd8f4e29a6d1124721fe3a3ac6e11aa8e
Juarez Martins <juniorbmx13@gmail.com>
2023-07-17 11:48:42 -0300

    "feat: CRUD category feat: tratamento de exceções"

--- src/main/java/com/ecoommercetreino/controller/CategoryController.java
+++ src/main/java/com/ecoommercetreino/controller/CategoryController.java
@@ -1,16 +1,16 @@
 package com.ecoommercetreino.controller;
 
 import com.ecoommercetreino.entities.Category;
+import com.ecoommercetreino.entities.Order;
 import com.ecoommercetreino.entities.User;
 import com.ecoommercetreino.services.CategoryService;
 import com.ecoommercetreino.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,4 +31,22 @@ public class CategoryController {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj){
+        obj = categoryService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Category> delete(@PathVariable Long id){
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
+        obj = categoryService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/ecoommercetreino/services/CategoryService.java
+++ src/main/java/com/ecoommercetreino/services/CategoryService.java
@@ -1,12 +1,19 @@
 package com.ecoommercetreino.services;
 
 import com.ecoommercetreino.entities.Category;
+import com.ecoommercetreino.entities.Order;
 import com.ecoommercetreino.entities.User;
 import com.ecoommercetreino.repositories.CategoryRepository;
+import com.ecoommercetreino.services.exceptions.DataBaseException;
+import com.ecoommercetreino.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -20,7 +27,36 @@ public class CategoryService {
     }
 
     public Category findById (Long id) {
-        Optional<Category> obj = categoryRepository.findById(id);
-        return obj.get();
+        try {
+            Optional<Category> obj = categoryRepository.findById(id);
+            return obj.get();
+        }catch(NoSuchElementException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+    public Category insert(Category obj) {
+        return categoryRepository.save(obj);
+    }
+    public void delete(Long id){
+        try{
+            categoryRepository.deleteById(id);
+        } catch (RuntimeException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public Category update(Long id, Category obj){
+        try {
+            Category entity = categoryRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return categoryRepository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Category entity, Category obj) {
+        entity.setName(obj.getName());
     }
 }
